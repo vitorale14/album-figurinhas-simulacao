ceddd4e29bbbf8d433cd8e9fb5a1cf29f161c6f5
vitorale14 <vito100alessadrine@gmail.com>
2026-02-03 11:47:43 -0300

    Exibe total de figurinhas no pacote e no álbum

--- main.php
+++ main.php
@@ -1,4 +1,5 @@
 <?php
+$pacote = [];
 //Gera o pacote
 for ($i = 1; $i <= 10; $i++) {
     $num = mt_rand(1, 1000);
@@ -19,5 +20,8 @@ foreach($pacote as $num) {
 }
 
 //var_dump($album);
+
+echo "Total no pacote: " . count($pacote) . PHP_EOL;
+echo "Total no álbum: " . count($album) . PHP_EOL;
 
 
